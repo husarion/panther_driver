746868fba4db9c0e3769c63eba6affe0a0511ff3
Khasreto <adam-krawczyk@outlook.com>
2020-08-31 17:39:07 +0200

    fixed ros_ip and master uri

--- system_config/launch_driver.sh
+++ system_config/launch_driver.sh
@@ -2,8 +2,10 @@
 
 eval "$(cat ~/.bashrc | tail -n +10)"
 source /opt/ros/$(rosversion -d)/setup.sh
+export ROS_IP=192.168.1.2
+export ROS_MASTER_URI=http://192.168.1.3:11311
 source /home/husarion/husarion_ws/devel/setup.sh
-roslaunch panther_driver webui.launch &
+roslaunch panther_driver &
 
 until ifconfig panther_can
 do
