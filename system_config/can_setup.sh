ddafba7af45ed9974aa69b6a57f088a965d0c4cb
Łukasz Mitka <luk.mitka@gmail.com>
2020-10-08 10:36:51 +0200

    Increase communication speed to 1000kbit/s

--- system_config/can_setup.sh
+++ system_config/can_setup.sh
@@ -4,10 +4,10 @@ modprobe can-raw
 modprobe slcan
 # cd /home/husarion/can-utils
 echo "slcan_attach..."
-slcan_attach -o -s4 -n panther_can /dev/ttyACM0
+slcan_attach -o -s8 -n panther_can /dev/ttyACM0
 sleep 5
 echo "slcand..."
-slcand -o -f -s4 -F /dev/ttyACM0 panther_can &
+slcand -o -f -s8 -F /dev/ttyACM0 panther_can &
 sleep 5
 echo "ifconfig..."
 ifconfig panther_can up
