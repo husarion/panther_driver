afaf7931ceac96adf9a02085fe0ebe7ee240f263
TopRamen1 <nicolas.duc@op.pl>
2022-05-13 10:12:35 +0200

    deleted panther system setup - moved to image building repo

--- panther_system_setup/eeprom/scripts/load_env_on_boot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Run script to generate export file
-sudo python3 /usr/local/sbin/config_loading.py
-
-# Set exec permission to export fille
-sudo chmod +x /etc/profile.d/panther_env_export.sh 
