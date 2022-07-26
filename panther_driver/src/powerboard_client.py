befb0cfeedfb0100c36c056a74bff19ace03e95f
TopRamen1 <nicolas.duc@op.pl>
2022-07-26 12:30:35 +0000

    Finished powerboard usage node, tested on rpi without robot

--- panther_driver/src/powerboard_client.py
+++ panther_driver/src/powerboard_client.py
@@ -7,10 +7,12 @@ from std_srvs.srv import Trigger, TriggerRequest, TriggerResponse
 import os
 import time
 import threading
+# import multiprocessing
 
 import RPi.GPIO as GPIO
 from gpiozero import Button, PWMOutputDevice, OutputDevice
 from signal import pause
+from std_msgs.msg import Bool
 
 # Define pin names
 VMOT_ON = 6
@@ -36,8 +38,9 @@ class PantherHardware:
         GPIO.setup(DRIVER_EN, GPIO.OUT, initial=0)
         GPIO.setup(E_STOP_RESET, GPIO.IN) # USED AS I/O
 
-        self.watchdog_pwm_thread = threading.Thread(name='watchdog_pwm proc', target=self.watchdog_pwm)
-        self.watchdog_pwm_thread.start()
+        self.watchdog_on = False
+        self.watchdog_pwm = PWMOutputDevice(WATCHDOG)
+        self.toggle_watchdog()
 
         self.soft_stop_thread = threading.Thread(name='soft stop proc', target=self.soft_stop)
         self.soft_stop_thread.start()
@@ -48,10 +51,26 @@ class PantherHardware:
         self.charger_enable_service = rospy.Service('/panther_hardware/charger_enable', SetBool, self.handle_charger_enable)
         self.disable_digital_service = rospy.Service('/panther_hardware/disable_digital_power', SetBool, self.handle_disable_digital_power)
         self.motors_enable_service = rospy.Service('/panther_hardware/motors_enable', SetBool, self.handle_motors_enable)
-        self.reset_estop_service = rospy.Service('/panther_hardware/reset_estop', Trigger, self.handle_reset_estop)
+        self.reset_e_stop_service = rospy.Service('/panther_hardware/reset_e_stop', Trigger, self.handle_reset_e_stop)
+        self.toggle_e_stop_service = rospy.Service('/panther_hardware/toggle_e_stop', Trigger, self.handle_toggle_e_stop)
+
+        self.e_stop_state_pub = rospy.Publisher('/panther_hardware/e_stop', Bool, queue_size=1)
+        self.timer_e_stop = rospy.Timer(rospy.Duration(0.1), self.publish_e_stop_state)
+
+        self.charger_state_pub = rospy.Publisher('/panther_hardware/charger_sens', Bool, queue_size=1)
+        self.timer_charger = rospy.Timer(rospy.Duration(0.5), self.publish_charger_state)
 
         rospy.spin()
 
+    def publish_e_stop_state(self, event=None):
+        msg = Bool()
+        msg.data = GPIO.input(E_STOP_RESET)
+        self.e_stop_state_pub.publish(msg)
+
+    def publish_charger_state(self, event=None):
+        msg = Bool()
+        msg.data = GPIO.input(CHRG_SENSE)
+        self.charger_state_pub.publish(msg)
 
     def handle_aux_power_enable(self, req: SetBoolRequest):
         return self.handle_set_bool_srv(req.data, AUX_PW_EN, "Aux power enable")
@@ -65,10 +84,10 @@ class PantherHardware:
     def handle_motors_enable(self, req: SetBoolRequest):
         return self.handle_set_bool_srv(req.data, DRIVER_EN, "Motors driver enable")
 
-    def handle_reset_estop(self, req: TriggerRequest):
+    def handle_reset_e_stop(self, req: TriggerRequest):
         # Read value before reset
-        estop_val = GPIO.input(E_STOP_RESET)
-        print(f"E-STOP state = {estop_val}")
+        e_stop_val = GPIO.input(E_STOP_RESET)
+        print(f"E-STOP state = {e_stop_val}")
 
         # Switch e-stop pin to output
         GPIO.setup(E_STOP_RESET, GPIO.OUT)
@@ -80,6 +99,11 @@ class PantherHardware:
         GPIO.setup(E_STOP_RESET, GPIO.IN)
 
         return response
+
+    def handle_toggle_e_stop(self, req: TriggerRequest):
+        self.toggle_watchdog()
+        return TriggerResponse(True, f"Watchdog on: {self.watchdog_on}")
+        
 
     def handle_set_bool_srv(self, reqest, pin, name):
         success = self.handle_gpio_write(reqest, pin, name)
@@ -119,11 +143,15 @@ class PantherHardware:
             return False
 
         
-    def watchdog_pwm(self):
-        watchdog_pwm = PWMOutputDevice(WATCHDOG)
-        freqency = 10
-        step = 1/freqency/2
-        watchdog_pwm.blink(on_time=step,off_time=step)
+    def toggle_watchdog(self):
+        if self.watchdog_on:
+            self.watchdog_pwm.off()
+            self.watchdog_on = False
+        else:
+            freqency = 10
+            step = 1/freqency/2
+            self.watchdog_pwm.blink(on_time=step,off_time=step)
+            self.watchdog_on = True
 
     def soft_stop(self):
         button = Button(SHDN_INIT, pull_up = False)
