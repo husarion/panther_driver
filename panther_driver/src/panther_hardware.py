9990d649c6b4429611e0663d9133a1811fcc5f1b
TopRamen1 <nicolas.duc@op.pl>
2022-08-12 16:00:56 +0200

    Added toogle of bot vmot and driver enable

--- panther_driver/src/panther_hardware.py
+++ panther_driver/src/panther_hardware.py
@@ -124,7 +124,12 @@ class PantherHardware:
         return self._handle_set_bool_srv(req.data, VDIG_OFF, "Digital power enable")
 
     def _motors_enable_cb(self, req: SetBoolRequest) -> SetBoolResponse:
-        return self._handle_set_bool_srv(req.data, DRIVER_EN, "Motors driver enable")
+        resp1 = self._handle_set_bool_srv(req.data, VMOT_ON, "Motors driver enable")
+        resp2 = self._handle_set_bool_srv(req.data, DRIVER_EN, "Motors driver enable")
+
+        if resp1.success and resp2.success:
+            return SetBoolResponse(True, resp1.message)
+        return SetBoolResponse(False, resp1.message)
 
     def _fan_enable_cb(self, req: SetBoolRequest) -> SetBoolResponse:
         return self._handle_set_bool_srv(req.data, FAN_SW, "Fan enable")
